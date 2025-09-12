d4d4572ce8010e8cd6595fcaf2a9402aa78f8d9f
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2025-09-12 21:20:36 +0700

    a

--- cell03/ex04/bis03/todo.js
+++ cell03/ex04/bis03/todo.js
@@ -12,7 +12,7 @@ function createTodo(text) {
 }
 
 function render() {
-    const $list = $('#ft_list');
+    const $list = $('#list');
     $list.empty();
     for (let i = 0; i < TodoList.length; i++) {
         $list.append(createTodo(TodoList[i]));
